e3a5373438e2427819f8aff10291bdbe805c74d6
saru <vagrant@s201803-17.localdomain>
2018-03-25 23:34:20 +0000

    changing html templates

--- app.py
+++ app.py
@@ -27,27 +27,25 @@ def graph():
     if request.method == 'GET':
 	return redirect('/index')
     elif len(appclone.vars['tickerinput']) <5:
-        try: # --- Get Quandl data ---
-	    # get date of one month ag
-	    t1 = str(date.today() - monthdelta.monthdelta(1))
-	    # request data from quandl using ticker input and date
-	    url = "https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json?ticker=%s&date.gte=%s&api_key=GcfBWgKuFpThaJ4uyNh2" % (appclone.vars['tickerinput'],t1)
-	    r = requests.get(url)
-	    #create dataframe in pandas
-	    cols = [x["name"] for x in (r.json()['datatable']['columns'])]
-	    tickerdf = pd.DataFrame((r.json()['datatable']['data']), columns=cols)
-	    # --- Create graph ---
-	    p = figure(plot_width=800, plot_height=400, x_axis_type="datetime")
-	    p.xaxis.axis_label = 'Date'
-	    p.yaxis.axis_label = 'Price'
-            p.line(x=data['date'], y=data['open'], color='lightblue', line_dash='dashed', legend='Opening Price')
-	    p.line(x=data['date'], y=data['close'], color='darkblue', legend='Closing Price')
-	    scriptp, divp = components(p)
-	    return render_template('graph2.html', ticker=appclone.vars['tickerinput'], script=script, div=div)
-	except:
-	    return render_template('graph2.html', placeholder='An error has occured.')
-    else: 
-	return render_template('graph2.html', placeholder='Stock ticker not recognized. Please try again.')
+        # --- Get Quandl data ---
+        # get date of one month ag
+	t1 = str(date.today() - monthdelta.monthdelta(1))
+	# request data from quandl using ticker input and date
+	url = "https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json?ticker=%s&date.gte=%s&api_key=GcfBWgKuFpThaJ4uyNh2" % (appclone.vars['tickerinput'],t1)
+	r = requests.get(url)
+	#create dataframe in pandas
+	cols = [x["name"] for x in (r.json()['datatable']['columns'])]
+	tickerdf = pd.DataFrame((r.json()['datatable']['data']), columns=cols)
+	# --- Create graph ---
+	p = figure(plot_width=800, plot_height=400, x_axis_type="datetime")
+	p.xaxis.axis_label = 'Date'
+	p.yaxis.axis_label = 'Price'
+        p.line(x=data['date'], y=data['open'], color='lightblue', line_dash='dashed', legend='Opening Price')
+	p.line(x=data['date'], y=data['close'], color='darkblue', legend='Closing Price')
+	script, div = components(p)
+	    return render_template('graph.html', script=script, div=div)
+     else: 
+	return redirect('/index.html')
 
 
 if __name__ == '__main__':
